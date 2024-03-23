6e4d9636a0c796c8b86a2794476a91f4613c31b5
daisymachiri <157162301+daisymachiri@users.noreply.github.com>
2024-03-23 20:48:35 +0200

    Added JS info

--- src/script.js
+++ src/script.js
@@ -32,3 +32,57 @@ function applyTransform (obj){
 
     obj.style.transform = "rotateX(" + (-tY) + "deg) rotateY(" + (tX) + "deg)";
 }
+
+function playSpin(yes){
+    ospin.style.animationPlayState = (yes?'running': 'paused');
+}
+var sX, sY, nX, nY, desX = 0,
+desY = 0,
+tX = 0,
+tY = 10;
+if (autoRotate){
+    var animationName = (rotateSpeed > 0 ? 'spin' : 'spinRevert');
+    ospin.style.animation = `${animationName} ${Math.abs(rotateSpeed)}s infinite linear `;
+}
+document.onpointerdown = function (e) {
+    clearInterval(odrag.timer);
+    e = e || window.event;
+    var sX = e.clientX,
+        sY = e.clientY;
+
+    this.onpointermove = function (e) {
+        e = e || window.event;
+        var nX = e.clientX,
+            nY = e.clientY;
+        var desX = nX - sX;
+        var desY = nY - sY;
+        tX += desX * 0.1;
+        tY += desY * 0.1;
+        applyTransform(odrag);
+        sX = nX;
+        sY = nY;
+    }
+    this.onpointerup = function (e) {
+        odrag.timer = setInterval(function(){
+            desX *= 0.95;
+            desY *= 0.95;
+            tX += desX * 0.1;
+            tY += desY * 0.1;
+            applyTransform(odrag);
+            playSpin(false);
+            if(Math.abs(desX) < 0.5 && Math.abs(desY) < 0.5){
+                clearInterval(odrag.timer);
+                playSpin(true);
+            }
+        }, 17);
+        this.onpointermove = this.onpointerup = null;
+    };
+    return false
+};
+
+document.onmousewheel = function (e) {
+    e = e || window.event;
+    var d = e.wheelDelta / 20 || -e.detail;
+    radius += d;
+    init(1);
+}
